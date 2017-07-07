e5b127e8655fe6c02627b37d0e30e89256d477c6
Ronald Vargas <ronaldvargas001@gmail.com>
2017-07-07 17:11:12 -0600

    b-day website
    the page takes age and name values and then says happ b-day, laso uses js to switch an image.

---
+++ js/script.js
@@ -0,0 +1,10 @@
+var name=prompt("Enter your Name: ");
+var age=prompt("Enter your age: ");
+
+
+function myFunction(img) {
+    alert('Hello '+name+' Happy '+age+' Birthday!!!');
+	document.getElementById('cake').src='off.jpg';
+
+}
+
